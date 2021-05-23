2f6ad9838298410ba9b32d6ffdeaa423201f5676
devlearning9874 <devlearning9874@gmail.com>
2021-05-23 18:15:02 +0530

    creating vue 10 apps

---
+++ vue-apps/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h1>DC Heroes</h1>
+ 
+ <div>
+  <ul >
+    <li v-for:="(hero, index) in dcHeroes" v-bind:key="index">{{index}}: {{hero.name}}</li>
+  </ul>
+ </div>
+ <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+     return {
+       dcHeroes: [{name:'Superman'},
+                   {name:'Batman'}, 
+                  {name:'Superwoman'},
+               {name:'Captain America'}
+       ],
+
+     }
+  },
+}
+</script>
+
+<style>
+
+</style>
